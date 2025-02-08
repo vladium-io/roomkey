529f99468ec5f707a944d4dc2bb364c3faa34602
vlad <vlad.andreuta@rogers.com>
2025-02-08 12:21:21 -0500

    created custom server for socket.io connections

---
+++ server.js
@@ -0,0 +1,30 @@
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+import { Server } from 'socket.io'
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    const server = createServer((req, res) => {
+        const parsedUrl = parse(req.url, true)
+        handle(req, res, parsedUrl)
+    });
+
+    const io = new Server(server);
+
+    io.on('connection', (socket) => {
+        console.log('client connected');
+
+        socket.on('disconnect', () => {
+            console.log('client disconnected');
+        });
+    });
+
+    server.listen(3000, (err) => {
+        if (err) throw err;
+        console.log('> Ready on http://localhost:3000');
+    });
+})
